28319f7aa86a645b998208ee4e2392774424a782
xhyrom <generalkubo@gmail.com>
2021-12-23 17:15:20 +0100

    creative stop

--- nexusblock-bukkit/src/hyro/lib/listeners/BlockDestroy.java
+++ nexusblock-bukkit/src/hyro/lib/listeners/BlockDestroy.java
@@ -2,6 +2,7 @@ package hyro.lib.listeners;
 
 import hyro.lib.Main;
 import hyro.lib.structures.Nexus;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
@@ -16,7 +17,7 @@ public class BlockDestroy implements Listener {
         Nexus nexus = Main.nexuses.get(eventBlock.getType().name());
 
         if(nexus != null) {
-            if(eventLocation.getBlockX() == nexus.location.getBlockX() && eventLocation.getBlockY() == nexus.location.getBlockY() && eventLocation.getBlockZ() == nexus.location.getBlockZ() && eventLocation.getWorld().getName() == nexus.location.getWorld().getName()) {
+            if(eventLocation.getBlockX() == nexus.location.getBlockX() && eventLocation.getBlockY() == nexus.location.getBlockY() && eventLocation.getBlockZ() == nexus.location.getBlockZ() && eventLocation.getWorld().getName() == nexus.location.getWorld().getName() && event.getPlayer().getGameMode() != GameMode.CREATIVE) {
                 event.setCancelled(true);
                 nexus.onHit(event.getPlayer());
             }
